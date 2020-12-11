7ca8445f8fdbcdf0818a53e95a5a3a6eb320e377
James Costello <jmc@Jamess-MacBook-Pro.local>
2020-12-11 15:13:08 -0700

    add larry

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
+const fs = require('fs');
+const path = require('path');
 const { animals } = require('./data/animals.json');
-
 const express = require('express');
-const { get } = require('http');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -45,11 +50,40 @@ function filterByQuery(query, animalsArray) {
   return filteredResults;
 }
 
+// function to find by id of array
 function findById(id, animalsArray) {
   const result = animalsArray.filter(animal => animal.id === id)[0];
   return result;
 }
 
+// function to create new animal
+function createNewAnimal(body, animalsArray) {
+  const animal = body;
+  animalsArray.push(animal);
+  fs.writeFileSync(
+    path.join(__dirname, './data/animals.json'),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+  return animal;
+}
+
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== 'string') {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== 'string') {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== 'string') {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+}
+
+// get results of json data && query using filter by query function
 app.get('/api/animals', (req, res) => {
   let results = animals;
   if (req.query) {
@@ -58,6 +92,7 @@ app.get('/api/animals', (req, res) => {
   res.json(results);
 });
 
+// get result === to id or send 500 error
 app.get('/api/animals/:id', (req, res) => {
   const result = findById(req.params.id, animals);
   if (result) {
@@ -67,6 +102,20 @@ app.get('/api/animals/:id', (req, res) => {
   }
 });
 
+app.post('/api/animals', (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
+
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+  }
+});
+
+// this uses const PORT to use either heroku default port or 3001 for local
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
