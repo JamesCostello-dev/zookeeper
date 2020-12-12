5ad5fa6856713d7fc59f193b7ac2e5d5c2bdaa73
James Costello <jmc@Jamess-MacBook-Pro.local>
2020-12-12 13:28:46 -0700

    GET and serve page data

--- server.js
+++ server.js
@@ -120,6 +120,15 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+
 // this uses const PORT to use either heroku default port or 3001 for local
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
